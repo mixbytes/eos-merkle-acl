94cdc2b9aac7ba6b415a41db5742248df3b90062
Algys Ievlev <algys.ievlev@gmail.com>
2018-07-02 17:38:37 +0300

    complete validation

--- merkle-acl.cpp
+++ merkle-acl.cpp
@@ -1,9 +1,8 @@
-#include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
 #include <eosiolib/crypto.h>
+#include <eosiolib/eosio.hpp>
 #include <eosiolib/types.hpp>
 #include <eosiolib/print.hpp>
-
 
 static constexpr uint64_t token_symbol = S(4, TEST); // precision, symbol
 
@@ -11,171 +10,133 @@ using namespace eosio;
 using eosio::asset;
 using std::make_pair;
 
+
 class simpletoken : public eosio::contract {
-   public:
-      simpletoken( account_name self )
-      :contract(self),_accounts( _self, _self), _mroots(_self, _self){}
+public:
+    simpletoken(account_name self)
+        : contract(self)
+        , _accounts(_self, _self)
+        , _mroots(_self, _self)
+    {
+    }
 
+    // @abi action
+    void merklemint(account_name from, checksum256 acc_name_hash, std::vector<checksum256> merkle_proof)
+    {
+        //require_auth( from );
+        //eosio_assert( merkle_proof.size() <= 255 );
 
-      // @abi action
-      void merklemint(account_name from, checksum256 acc_name_hash, std::vector<checksum256> merkle_proof) {
-			// [FIXME] mocks
-      	//require_auth( from );
-			//eosio_assert( merkle_proof.size() <= 255 );
-			checksum256 hz_root = traversemerkle(acc_name_hash, merkle_proof);
+        checksum256 hz_root = traversemerkle(acc_name_hash, merkle_proof);
 
+        auto const & _merkle_acl_root = _mroots.get(N(mroot));
 
-			auto _merkle_acl_root = _mroots.get(N(mroot));
-			/*
-			for (char i = 0; i < 32; i++) {
-				eosio::print((char)_merkle_acl_root.mroot.hash[i]);
-			}
-			eosio::print("|");
-			for (char i = 0; i < 32; i++) {
-				eosio::print((char)hz_root.hash[i]);
-			}
-			eosio::print("|");
-			*/
-			
+        eosio_assert(hz_root == _merkle_acl_root.mroot, "Account is not ok");
 
+        eosio::print("Account is ok, minted\n");
+        issue(from, _merkle_issue_amount);
+    }
 
-			// [FIXME] fucking MAIN mock
-			hz_root = _merkle_acl_root.mroot;
+    // @abi action
+    void transfer(account_name from, account_name to, asset quantity)
+    {
+        require_auth(from);
+        eosio_assert(quantity.symbol == token_symbol, "wrong symbol");
 
-			if (memcmp(&hz_root, &_merkle_acl_root.mroot, sizeof(checksum256)) == 0) {
-				eosio::print("Account is ok, minted");
-				issue(from, _merkle_issue_amount);
-				return;
-			}
-			eosio::print("Account is not ok");
-  		}
+        const auto& fromacnt = _accounts.get(from);
+        eosio_assert(fromacnt.balance >= quantity, "overdrawn balance");
+        _accounts.modify(fromacnt, from, [&](auto& a) { a.balance -= quantity; });
 
-      // @abi action
-      void transfer( account_name from, account_name to, asset quantity ) {
-         require_auth( from );
-         eosio_assert( quantity.symbol == token_symbol, "wrong symbol" );
+        add_balance(from, to, quantity);
+    }
 
-         const auto& fromacnt = _accounts.get( from );
-         eosio_assert( fromacnt.balance >= quantity, "overdrawn balance" );
-         _accounts.modify( fromacnt, from, [&]( auto& a ){ a.balance -= quantity; } );
+    // @abi action
+    void issue(account_name to, asset quantity)
+    {
+        require_auth(_self);
+        eosio_assert(quantity.symbol == token_symbol, "wrong symbol");
 
-         add_balance( from, to, quantity );
-      }
+        add_balance(_self, to, quantity);
+    }
 
-      // @abi action
-      void issue( account_name to, asset quantity ) {
-         require_auth( _self );
-         eosio_assert( quantity.symbol == token_symbol, "wrong symbol" );
+    // @abi action
+    void mrklsetprms(checksum256 merkle_root)
+    {
+        require_auth(_self);
+        auto toitr = _mroots.find(N(mroot));
+        if (toitr == _mroots.end()) {
+            _mroots.emplace(_self, [&](auto& a) {
+                a.id = N(mroot);
+                a.mroot = merkle_root;
+            });
+        } else {
+            _mroots.modify(toitr, 0, [&](auto& a) {
+                a.id = N(mroot);
+                a.mroot = merkle_root;
+            });
+        }
+    }
 
-         add_balance( _self, to, quantity );
-      }
+private:
+    // [TODO] @abi hz
+    asset _merkle_issue_amount = asset(100, token_symbol);
 
-      // @abi action
-      void mrklsetprms(checksum256 merkle_root) {
-         require_auth( _self );
-         auto toitr = _mroots.find(N(mroot));
-         if( toitr == _mroots.end() ) {
-           _mroots.emplace( _self, [&]( auto& a ) {
-				  a.id = N(mroot);
-              a.mroot = merkle_root;
-           });
-         } else {
-           _mroots.modify( toitr, 0, [&]( auto& a ) {
-				  a.id = N(mroot);
-              a.mroot = merkle_root;
-           });
-         }
-      }
+    // @abi table
+    struct mroot {
+        uint64_t id;
+        checksum256 mroot; // hello JS !
 
-   private:
-		// [TODO] @abi hz
-      asset _merkle_issue_amount = asset(100, token_symbol);
+        uint64_t primary_key() const { return N(mroot); }
+    };
 
-		// @abi table
-		struct mroot {
-	  		uint64_t id;
-			checksum256 mroot; // hello JS !
-			
-         uint64_t primary_key()const { return N(mroot); }
-		};
-		
-      // @abi table
-      struct account {
-         account_name owner;
-         eosio::asset balance;
+    // @abi table
+    struct account {
+        account_name owner;
+        eosio::asset balance;
 
-         uint64_t primary_key()const { return owner; }
-      };
+        uint64_t primary_key() const { return owner; }
+    };
 
-      eosio::multi_index<N(account), account> _accounts;
-      eosio::multi_index<N(mroot), mroot> _mroots;
+    eosio::multi_index<N(account), account> _accounts;
+    eosio::multi_index<N(mroot), mroot> _mroots;
 
-      void add_balance( account_name payer, account_name to, asset q ) {
-         auto toitr = _accounts.find( to );
-         if( toitr == _accounts.end() ) {
-           _accounts.emplace( payer, [&]( auto& a ) {
-              a.owner = to;
-              a.balance = q;
-           });
-         } else {
-           _accounts.modify( toitr, 0, [&]( auto& a ) {
-              a.balance += q;
-              eosio_assert( a.balance >= q, "overflow detected" );
-           });
-         }
-      }
-		bool is_canonical_left(const checksum256& val) {
-			checksum256 canonical_l = val;
-			// for(char i=0; i < 7; i++) {
-			//	canonical_l.hash[i] &= 0x00;
-			// } 
-			canonical_l.hash[7] &= 0x80;
-			if (canonical_l.hash[7] == 0) {
-				return true;
-			}
-			return false;                                                                                                                            
-		}                                                                                                                                                             
-																																																						  
-		checksum256 make_canonical_left(const checksum256& val) {                                                                                                     
-			checksum256 canonical_l = val;
-			for(char i=0; i < 7; i++) {
-				canonical_l.hash[i] &= 0xFF;
-			} 
-			canonical_l.hash[7] &= 0x7F;                                                                                                                            
-			return canonical_l;                                                                                                                                        
-		}                                                                                                                                                             
-																																																						  
-		checksum256 make_canonical_right(const checksum256& val) {                                                                                                    
-			checksum256 canonical_r = val;                                                                                                                             
-			for(char i=0; i < 7; i++) {
-				canonical_r.hash[i] |= 0x00;
-			} 
-			canonical_r.hash[7] |= 0x80;;                                                                                                                            
-			return canonical_r;                                                                                                                                        
-		}     
-																																																								  
-   	inline char* make_canonical_pair(const checksum256& l, const checksum256& r) {
-		 	char len = sizeof(checksum256);
-			static char buf[sizeof(checksum256) * 2];
-			memcpy(&buf, &l, len);
-			memcpy(&buf + len, &r, len);
-			return buf;
-   	};
-          
-	 	checksum256 traversemerkle(checksum256& leaf, std::vector<checksum256>& ids) {
-			checksum256 current = leaf;
-			unsigned char len = sizeof(checksum256);
- 
-			for (checksum256& p: ids ) {
-      		if (!is_canonical_left(p)) {
-         		sha256((char *)make_canonical_pair(p, current), len * 2, &current);
-					
-      		} else {                                                                                                                                                
-         		sha256((char *)make_canonical_pair(current, p), len * 2, &current);
-				}
-      	}
-			return current;                                                                                                                                                       
-   	}                                                                                                                                                          
+    void add_balance(account_name payer, account_name to, asset q)
+    {
+        auto toitr = _accounts.find(to);
+        if (toitr == _accounts.end()) {
+            _accounts.emplace(payer, [&](auto& a) {
+                a.owner = to;
+                a.balance = q;
+            });
+        } else {
+            _accounts.modify(toitr, 0, [&](auto& a) {
+                a.balance += q;
+                eosio_assert(a.balance >= q, "overflow detected");
+            });
+        }
+    }
+
+    char* make_canonical_pair(const checksum256 & l, const checksum256 & r)
+    {
+        static char buf[64];
+        memcpy(buf, &l.hash, 32);
+        memcpy(buf + 32, &r.hash, 32);
+        return buf;
+    };
+
+    checksum256 traversemerkle(const checksum256 & leaf, const std::vector<checksum256> & proof)
+    {
+        checksum256 current = leaf;
+
+        for (auto && el: proof) {
+            if (std::less<checksum256>()(current, el)) {
+                sha256(make_canonical_pair(current, el), 64, &current);
+            } else {
+                sha256(make_canonical_pair(el, current), 64, &current);
+            }
+        }
+
+        return current;
+    }
 };
 
-EOSIO_ABI( simpletoken, (merklemint)(transfer)(issue)(mrklsetprms) )
+EOSIO_ABI(simpletoken, (merklemint)(transfer)(issue)(mrklsetprms))
--- test/merkle-acl-test.js
+++ test/merkle-acl-test.js
@@ -4,172 +4,177 @@ const axios = require('axios');
 const Eos = require('eosjs');
 const fs = require('fs');
 //const merkle = require('merkle');
-//const crypto = require('crypto');  
+//const crypto = require('crypto');
 const MerkleTree = require('./merkleTree');
 
+function makeid()
+{
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
 
-function makeid() {
-  	var text = "";
-  	var possible = "abcdefghijklmnopqrstuvwxyz";
-
-  	for (var i = 0; i < 12; i++) {
-    	text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-  return text;
+    for (var i = 0; i < 12; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 }
 
+function mockValidator(root, leaf, proof) {
+    let current = leaf;
 
-common.run({name: 'Create merkle-acl contract', version: '0.0.1'})
+    proof.forEach((el) => {
+        current = Buffer.from(Eos.modules.ecc.sha256(Buffer.concat([current, el].sort(Buffer.compare))), "hex");
+    });
+
+    console.log("current: ", current);
+    console.log("root: ", root);
+    return Buffer.compare(root, current);
+}
+
+common.run({ name : 'Create merkle-acl contract', version : '0.0.1' })
     .then(async (app) => {
-		//const node_url = 'https://frodo.eosnode.smartz.io:18888';
-		const node_url = 'http://127.0.0.1:8888';
+        //const node_url = 'https://frodo.eosnode.smartz.io:18888';
+        const node_url = 'http://127.0.0.1:8888';
 
-		// [TODO] make all async normally, not like this shit		
-        let getInfoResp = await axios.get(node_url+'/v1/chain/get_info').catch(function (error) {
-			throw("EOS node at '" + node_url + "is not available");
-		 });
-
-		// [TODO] output much more info about error, (connection, response) and move to separate function pingNode
-		if (getInfoResp.status !== 200) {
-            throw('EOS node not returned 200 OK');
-		}
-
-		const eosio_wif = "5KbuFQTX8zb31NNfwV87baBaJXH8Ukt2ix3MseraRZtniFCPm6W";
-		const eosio_pub = Eos.modules.ecc.privateToPublic(eosio_wif);
-
-		let eos = await Eos({
-            chainId: getInfoResp.data.chain_id,
-            keyProvider: eosio_wif,
-            httpEndpoint: node_url,
-				binaryen: binaryen,
+        // [TODO] make all async normally, not like this shit
+        let getInfoResp = await axios.get(node_url + '/v1/chain/get_info').catch(function(error) {
+            throw ("EOS node at '" + node_url + "is not available");
         });
 
-		let contractUser = makeid();
-		let contractWif = Eos.modules.ecc.seedPrivate(Math.random().toString());
-		let contractPub = Eos.modules.ecc.privateToPublic(contractWif);
-		const account = await eos.newaccount({
-      		creator: "pril",
-      		name: contractUser,
-      		owner: contractPub,
-      		active: contractPub,
-    	});
+        // [TODO] output much more info about error, (connection, response) and move to separate function pingNode
+        if (getInfoResp.status !== 200) {
+            throw ('EOS node not returned 200 OK');
+        }
 
-		let ceos = await Eos({
-            chainId: getInfoResp.data.chain_id,
-            keyProvider: contractWif,
-            httpEndpoint: node_url,
-				binaryen: binaryen,
-      });
+        const eosio_wif = "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3";
+        const eosio_pub = Eos.modules.ecc.privateToPublic(eosio_wif);
 
-		let wasm = await fs.readFileSync('../build/merkle-acl.wast');
-		if (typeof(wasm) == 'undefined') {
-			throw("Error reading .wast file");
-		};
+        let eos = Eos({
+            chainId : getInfoResp.data.chain_id,
+            keyProvider : eosio_wif,
+            httpEndpoint : node_url,
+            binaryen : binaryen,
+        });
 
-		let abi_json = await fs.readFileSync('../build/merkle-acl.abi');
-		if (typeof(abi_json) == 'undefined') {
-			throw("Error reading .abi file");
-		};
+        let contractUser = makeid();
+        let contractWif = "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3";
+        let contractPub = Eos.modules.ecc.privateToPublic(contractWif);
+        const account = await eos.newaccount({
+            creator : "eosio",
+            name : contractUser,
+            owner : contractPub,
+            active : contractPub,
+        });
 
-		// [NOTE] HZ WTF?
-		abi = JSON.parse(abi_json);
-		// abi.version = "eosio::abi/1.0";
+        console.log("CONTRACT: ", contractUser);
 
-		try {
-			await ceos.setcode(contractUser, 0, 0, wasm);
-			await ceos.setabi(contractUser, abi);
-		}
-		catch(e) {
-			throw("Set code failed: " + e);
-		}
-	
-		let contract = null;
-		await ceos.contract(contractUser).then(c => contract = c);
-		await(console.log("Created user '" + contractUser + "' with pubkey: " + contractPub + ", set merkle contract"));
+        let ceos = Eos({
+            chainId : getInfoResp.data.chain_id,
+            keyProvider : contractWif,
+            httpEndpoint : node_url,
+            binaryen : binaryen,
+        });
 
-		console.log("\n\nBegin to generate users");
-		let N=10;
-		let accounts_names = [];
-		let accounts = [];
-		for (var i = 0; i < N; i++) {
-			const wif = Eos.modules.ecc.seedPrivate(Math.random().toString());
-    		const pub = Eos.modules.ecc.privateToPublic(wif);
-			const user = makeid();
-			// sorryyyy
-			accounts_names.push(user);
-			accounts.push({ name: user, pub: pub, priv: wif });
-	
-			const account = await eos.newaccount({
-      		creator: "pril",
-      		name: user,
-      		owner: pub,
-      		active: pub
-    		});
-			await(console.log("Created user '" + user));
-		}
+        let wasm = fs.readFileSync('../build/merkle-acl.wast');
+        if (typeof (wasm) == 'undefined') {
+            throw ("Error reading .wast file");
+        };
 
-		let allowed_accounts = [];
-		let not_allowed_accounts = [];
-		for (let i=0; i < accounts_names.length; i++) {
-			if (i % 2 == 0) {
-				allowed_accounts.push(accounts_names[i]);
-			} else {
-				not_allowed_accounts.push(accounts_names[i]);
-			}
-		}
-		console.log("Split users, " + allowed_accounts.length + " allowed, " + not_allowed_accounts.length + " not allowed");
+        let abi_json = fs.readFileSync('../build/merkle-acl.abi');
+        if (typeof (abi_json) == 'undefined') {
+            throw ("Error reading .abi file");
+        };
 
+        // [NOTE] HZ WTF?
+        abi = JSON.parse(abi_json);
+        // abi.version = "eosio::abi/1.0";
 
-		// [FIXME] MAIN place - this .js module is not compatible with proofs, made in libraries/chain/merkle.cpp
-		// replace with compatible one, rewrite .cpp implementation, or wait until EOS will include merkle methods 
-		// in WASM interface
+        try {
+            await ceos.setcode(contractUser, 0, 0, wasm);
+            await ceos.setabi(contractUser, abi);
+        } catch (e) {
+            throw ("Set code failed: " + e);
+        }
 
-		let merkleTree = await new MerkleTree(allowed_accounts);
-		const merkleHexRoot = await merkleTree.getHexRoot().substring(2);
-		const merkleRoot = merkleTree.getRoot();
-		try {                                                                                                                                                 
-			let trx = await contract.mrklsetprms({merkle_root: merkleRoot}, {authorization: contractUser});
-			await console.log("Built merkle tree from allowed accounts, root: " + merkleHexRoot + " was written to contract");
-	  	}                                                                                                                                                     
-	  	catch (e) {                                                                                                                                           
-			console.log(e);                                                                                                                                   
-	  	} 
-		
+        let contract = null;
+        await ceos.contract(contractUser).then(c => contract = c);
+        console.log("Created user '" + contractUser + "' with pubkey: " + contractPub + ", set merkle contract");
 
-		for (let i=0; i< allowed_accounts.length; i++) {
-			let a = allowed_accounts[i];
-			let hh = await Buffer.from(Eos.modules.ecc.sha256(a), "hex");
-			let mProof = await merkleTree.getProof(a);                                                                   
-			//console.log(a);
-			//console.log(hh);
-			//console.log(mProof);
-			try {                                                                                                                                                 
-				let trx = await contract.merklemint({from: a, acc_name_hash: hh, merkle_proof: mProof}, {authorization: contractUser});
-			}                                                                                                                                             
-			catch (e) {                                                                                                                                           
-				console.log(e);                                                                                                                                   
-			}
-		}
+        console.log("\n\nBegin to generate users");
+        let N = 10;
+        let accounts_names = [];
+        let accounts = [];
+        for (var i = 0; i < N; i++) {
+            const wif = Eos.modules.ecc.seedPrivate(Math.random().toString());
+            const pub = Eos.modules.ecc.privateToPublic(wif);
+            const user = makeid();
+            // sorryyyy
+            accounts_names.push(user);
+            accounts.push({ name : user, pub : pub, priv : wif });
 
+            const account = await eos.newaccount({
+                creator : "eosio",
+                name : user,
+                owner : pub,
+                active : pub
+            });
+            console.log("Created user '" + user);
+        }
 
-		for (let i=0; i< not_allowed_accounts.length; i++) {
-			let a = not_allowed_accounts[i];
-			let hh = Buffer.from(Eos.modules.ecc.sha256(a), "hex");
-			//let mProof = await merkleTree.getProof(a);                                                                   
-			//console.log(a);
-			//console.log(hh);
-			//console.log(mProof);
-			try {                                                                                                                                                 
-				//let trx = await contract.merklemint({from: a, acc_name_hash: hh, merkle_proof: mProof}, {authorization: contractUser});
-			}                                                                                                                                             
-			catch (e) {                                                                                                                                           
-				console.log(e);                                                                                                                                   
-			}
-		}
+        let allowed_accounts = [];
+        let not_allowed_accounts = [];
+        for (let i = 0; i < accounts_names.length; i++) {
+            if (i % 2 == 0) {
+                allowed_accounts.push(accounts_names[i]);
+            } else {
+                not_allowed_accounts.push(accounts_names[i]);
+            }
+        }
+        console.log("Split users, " + allowed_accounts.length + " allowed, " + not_allowed_accounts.length + " not allowed");
 
+        // [FIXME] MAIN place - this .js module is not compatible with proofs, made in libraries/chain/merkle.cpp
+        // replace with compatible one, rewrite .cpp implementation, or wait until EOS will include merkle methods
+        // in WASM interface
 
+        let merkleTree = await new MerkleTree(allowed_accounts);
+        const merkleHexRoot = await merkleTree.getHexRoot().substring(2);
+        const merkleRoot = merkleTree.getRoot();
+        try {
+            let trx = await contract.mrklsetprms({ merkle_root : merkleRoot }, { authorization : contractUser });
+            console.log("Built merkle tree from allowed accounts, root: " + merkleHexRoot + " was written to contract");
+        } catch (e) {
+            console.log(e);
+        }
 
+        for (let i = 0; i < allowed_accounts.length; i++) {
+            let a = allowed_accounts[i];
+            let hh = Buffer.from(Eos.modules.ecc.sha256(a), "hex");
+            let mProof = merkleTree.getProof(a);
+            //console.log(a);
+            //console.log(hh);
+            //console.log(mProof);
+            try {
+        //        console.log("VALIDATE: ", mockValidator(merkleRoot, hh, mProof));
+                let trx = await contract.merklemint({ from : a, acc_name_hash : hh, merkle_proof : mProof }, { authorization : contractUser });
+                console.log("Account is Ok:", a);
+            } catch (e) {
+                console.log(e);
+            }
+        }
 
-	}).catch(function(e) {
-		console.log(e);
-	});
+        for (let i = 0; i < not_allowed_accounts.length; i++) {
+            let a = not_allowed_accounts[i];
+            let hh = Buffer.from(Eos.modules.ecc.sha256(a), "hex");
+            //let mProof = await merkleTree.getProof(a);
+            //console.log(a);
+            //console.log(hh);
+            //console.log(mProof);
+            try {
+                //let trx = await contract.merklemint({from: a, acc_name_hash: hh, merkle_proof: mProof}, {authorization: contractUser});
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    })
+    .catch(function(e) {
+        console.log(e);
+    });
